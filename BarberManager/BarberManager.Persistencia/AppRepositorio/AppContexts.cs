161fb7a101122ab004d01169703f7e0d4f1a470c
Hector Becerra <hlbecerra@hotmail.com>
2022-08-28 09:22:38 -0500

    Creacion de la arquitectura del proyecto

---
+++ BarberManager/BarberManager.Persistencia/AppRepositorio/AppContexts.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberManager.Persistencia.AppRepositorio
+{
+    public class AppContexts : DbContext
+    {
+        
+    }
+}
