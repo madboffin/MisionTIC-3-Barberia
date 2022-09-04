10cb820c41a99586b9c43314931e79e005def5ce
madboffin <manuelhuertasfl@gmail.com>
2022-09-04 09:37:28 -0500

    add gestionPersonas/barbero, add Reporte/reporte, pages from the frontend

---
+++ BarberManager/BaberManager.Frontend/Pages/GestionPersonas/Barbero/Barbero.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaberManager.Frontent
+{
+    public class BarberoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BarberManager/BaberManager.Frontend/Pages/Reporte/Reporte.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaberManager.Frontent
+{
+    public class ReporteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
