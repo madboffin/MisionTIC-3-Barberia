b423da6c29cc00c90912c5861d9d273bbc87046a
madboffin <manuelhuertasfl@gmail.com>
2022-09-08 09:26:49 -0500

    fix domain entities, and persistence AppContext, migrate to new server

--- BarberManager/BarberManager.Persistencia/AppRepositorio/AppContexts.cs
+++ BarberManager/BarberManager.Persistencia/AppRepositorio/AppContexts.cs
@@ -4,28 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using BarberManager.Dominio;
-
-namespace BarberManager.Persistencia.AppRepositorio
+namespace BarberManager.Persistencia
 {
     public class AppContexts : DbContext
     {
-        public DbSet<Barbero> Barbero { get; set; }
-        public DbSet<DetalleServicio> DetalleServicio { get; set; }
-        public DbSet<Rol> Rol { get; set; }
-        public DbSet<Servicio> Sevicio { get; set; }
-        public DbSet<Usuario> Usuario { get; set; }
-        public DbSet<Venta> Venta { get; set; }
-       
-protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public DbSet<Barbero> Barberos { get; set; }
+        public DbSet<DetalleServicio> DetalleServicios { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+        public DbSet<Servicio> Sevicios { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder
-               .UseSqlServer("Server=20.168.13.51; Database=barberia; user=sa; password=P3g4sus.2022*;");
-               // .UseSqlServer("Server=LAPTOP-5I9PKCH7; Database=HospiEnCasa; Integrated Security=True;") ;
+               .UseSqlServer("Server=tcp:s-east.database.windows.net,1433;Initial Catalog=barber_manager;Persist Security Info=False;User ID=presume5479;Password=N!ftQ^5gbxxBwV2yw#&Fk2H;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
-
-
     }
 }
