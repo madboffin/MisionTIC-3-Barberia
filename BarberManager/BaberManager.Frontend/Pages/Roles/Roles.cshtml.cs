ede45afdda3a75abe30886d58bb819343c3c1619
Eduardo Ortiz <ed.hilander2@gmail.com>
2022-09-18 19:01:48 -0500

    Crear página de Roles

--- BarberManager/BaberManager.Frontend/Pages/Roles/Roles.cshtml.cs
+++ BarberManager/BaberManager.Frontend/Pages/Roles/Roles.cshtml.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BarberManager.Dominio;
+using BarberManager.Persistencia;
 
-namespace BaberManager.Frontent
+
+namespace BaberManager.Frontend
 {
     public class RolesModel : PageModel
     {
+        private static ICRUD<Rol> _repositorioRol = new RepositorioRol(new BarberManager.Persistencia.AppContexts());
+        public List<Rol> listaRoles = new List<Rol>();
         public void OnGet()
         {
+            listaRoles = _repositorioRol.Consultar().ToList();
         }
     }
 }
+
---
+++ BarberManager/BaberManager.Frontend/Pages/Roles/RolesEditar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaberManager.Frontend
+{
+    public class RolesEditarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
