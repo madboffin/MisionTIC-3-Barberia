6854b66475c6be22939452d1e7974b7280f89a67
Eduardo Ortiz <ed.hilander2@gmail.com>
2022-09-22 19:54:15 -0500

    Página roles editar parcial

--- BarberManager/BaberManager.Frontend/Pages/Roles/RolesEditar.cshtml.cs
+++ BarberManager/BaberManager.Frontend/Pages/Roles/RolesEditar.cshtml.cs
@@ -1,12 +1,54 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Web;
+using BarberManager.Dominio;
+using BarberManager.Persistencia;
 
 namespace BaberManager.Frontend
 {
     public class RolesEditarModel : PageModel
     {
-        public void OnGet()
+        private static ICRUD<Rol> _repositorioRol = new RepositorioRol(
+            new BarberManager.Persistencia.AppContexts()
+        );
+        public Rol rol = new Rol();
+
+        public IActionResult OnGet(int Id)
         {
+            var rol = _repositorioRol.Buscar(Id);
+            if (rol == null)
+            {
+                return RedirectToPage("./Roles");
+            }
+            else
+            {
+                Console.WriteLine(rol.Id + rol.Nombre);
+                return Page();
+            }
+        }
+
+        public IActionResult Onpost()
+        {
+           
+            Console.WriteLine(rol.Id + rol.Nombre);
+
+            string id = Request.Form["idRol"];
+            string nombre = Request.Form["nombreRol"];
+            rol.Nombre = nombre;
+            Console.WriteLine(rol.Id + rol.Nombre);
+
+            var RolAdicionado = _repositorioRol.Actualizar(rol);
+            if (RolAdicionado == null)
+            {
+                ViewData["mensaje"] = "No se puedo actualizar";
+                Console.WriteLine("Error al guardar el Rol");
+            }
+            else
+            {
+                ViewData["mensaje"] = "Guardado exitosamente";
+                Console.WriteLine("Guardado exitosamente");
+            }
+            return RedirectToPage("./Roles");
         }
     }
 }
