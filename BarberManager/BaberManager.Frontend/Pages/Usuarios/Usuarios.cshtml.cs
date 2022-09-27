cd56b05830a98ee05fbf8f0ef591e836b92a21ec
madboffin <manuelhuertasfl@gmail.com>
2022-09-26 21:10:46 -0500

    agregar lista de usuarios

--- BarberManager/BaberManager.Frontend/Pages/Usuarios/Usuarios.cshtml.cs
+++ BarberManager/BaberManager.Frontend/Pages/Usuarios/Usuarios.cshtml.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BarberManager.Persistencia;
+using BarberManager.Dominio;
 
-namespace BaberManager.Frontent
-{
-    public class UsuariosModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace BaberManager.Frontent {
+    public class UsuariosModel : PageModel {
+        private static ICRUD<Usuario> _repositorioUsuario = new RepositorioUsuario(new BarberManager.Persistencia.AppContexts());
+        private static ICRUD<Rol> _repositorioRol = new RepositorioRol(new BarberManager.Persistencia.AppContexts());
+        public List<Usuario> listaUsuarios {get; set;} = new List<Usuario>();
+        public void OnGet() { 
+            listaUsuarios = _repositorioUsuario.Consultar().ToList();
+        }
+        public void OnPost() {
         }
     }
 }
