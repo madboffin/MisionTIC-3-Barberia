2243650b3c39d37a1c37e5a7a9fa08d4ce4e89e3
madboffin <manuelhuertasfl@gmail.com>
2022-09-27 20:49:58 -0500

    incluir objetos que son propiedades de otros en los repositorios, atraves de linq .Include

--- BarberManager/BarberManager.Persistencia/AppRepositorio/RepositorioRol.cs
+++ BarberManager/BarberManager.Persistencia/AppRepositorio/RepositorioRol.cs
@@ -2,20 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using BarberManager.Dominio;
 
-namespace BarberManager.Persistencia
-{
-    public class RepositorioRol : ICRUD<Rol>
-    {
+namespace BarberManager.Persistencia {
+    public class RepositorioRol : ICRUD<Rol> {
         private readonly AppContexts appContexts;
-        public RepositorioRol(AppContexts appContexts)
-        {
+        public RepositorioRol(AppContexts appContexts) {
             this.appContexts = appContexts;
         }
 
-        public Rol Actualizar(Rol obj)
-        {
+        public Rol Actualizar(Rol obj) {
             var rolEncontrado = Buscar(obj.Id);
             if (rolEncontrado != null)
             {
@@ -26,25 +23,21 @@ namespace BarberManager.Persistencia
             return rolEncontrado;
         }
 
-        public Rol Adicionar(Rol obj)
-        {
+        public Rol Adicionar(Rol obj) {
             var rol = appContexts.Roles.Add(obj);
             appContexts.SaveChanges();
             return rol.Entity;
         }
 
-        public Rol Buscar(int Id)
-        {
+        public Rol Buscar(int Id) {
             return appContexts.Roles.FirstOrDefault(r => r.Id == Id);
         }
 
-        public IEnumerable<Rol> Consultar()
-        {
+        public IEnumerable<Rol> Consultar() {
             return appContexts.Roles;
         }
 
-        public int Eliminar(int Id)
-        {
+        public int Eliminar(int Id) {
             int result = 0;
             var rolEncontrado = Buscar(Id);
             if (rolEncontrado == null)
