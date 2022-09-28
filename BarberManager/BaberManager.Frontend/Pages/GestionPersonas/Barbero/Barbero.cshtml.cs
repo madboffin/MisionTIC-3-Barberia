b3860ee6f00011cff3f5bb555b537cc4df5014cd
Eduardo Ortiz <ed.hilander2@gmail.com>
2022-09-27 22:46:18 -0500

    Página Barberos Crear

--- BarberManager/BaberManager.Frontend/Pages/GestionPersonas/Barbero/Barbero.cshtml.cs
+++ BarberManager/BaberManager.Frontend/Pages/GestionPersonas/Barbero/Barbero.cshtml.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BarberManager.Dominio;
+using BarberManager.Persistencia;
 
 namespace BaberManager.Frontend
 {
     public class BarberoModel : PageModel
     {
-        public void OnGet()
-        {
+        private static ICRUD<Barbero> _repositorioBarbero = new RepositorioBarbero(new BarberManager.Persistencia.AppContexts());
+        public List<Barbero> listaBarberos = new List<Barbero>();
+        public void OnGet() {
+            listaBarberos = _repositorioBarbero.Consultar().ToList();
         }
+        
     }
 }
